76b217ccf0149b8dfa73d77b542149f63c337506
pengchihui1 <pengchihui1@gmail.com>
2021-12-28 22:19:42 +0800

    test

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const product = require("./api/product");
 
 const app = express();
 
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.use(cors())
 app.use(express.json({ extended: false }));
 
