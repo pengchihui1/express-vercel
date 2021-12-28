ae5aa6fa4d81d9539ac3c286a01ebcab2bfd178b
pengchihui1 <51830726+pengchihui1@users.noreply.github.com>
2021-12-28 22:44:54 +0800

    test

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.use(cors())
    // next();
 //});
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Headers', 'accept, authorization, content-type, x-requested-with');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Origin', req.header('origin'));
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Headers', 'accept, authorization, content-type, x-requested-with');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//     res.setHeader('Access-Control-Allow-Origin', req.header('origin'));
+//     next();
+// });
 
 // app.use(express.json({ extended: true }));
 
